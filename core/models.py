eaa778b3bc1adc8f993383de3cdc71b6a6a2b873
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-03-20 12:11:32 -0400

    set up post/home view and templates

--- core/models.py
+++ core/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth import get_user_model
+
 # Create your models here.
 # Comments belong to a user, the author, and a post.
 # Users have many posts, comments, and votes.
 
-User = get_user_model()
-
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100, null=True, blank=True)
+    title = models.CharField(max_length=150, null=True, blank=True)
     author = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True, blank=True)
     date_posted = models.DateTimeField(
         'Date Published', auto_now_add=True, null=True)
     date_updated = models.DateTimeField(null=True, auto_now=True)
-    votes = models.ManyToManyField(to='Profile', related_name='posts')
+    votes = models.ManyToManyField(
+        to='Profile', related_name='posts', blank=True)
     url = models.URLField(max_length=250, null=True)
 
     # slug = AutoSlugField(unique=True, populate_from="title", blank=True, null=True)
@@ -23,9 +24,19 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['-date_posted']
+
+    def get_absolute_url(self):
+        return reverse("core-post", args=(self.pk, ))
+
 
 class Profile(models.Model):
+    User = get_user_model()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Comment(models.Model):
